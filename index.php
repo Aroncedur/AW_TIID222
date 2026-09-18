f7b5d13640eee22645aabce444261ea9f7b80ab9
Aaron Eduardo <125054686@upq.edu.mx>
2026-09-17 19:45:46 -0600

    ËDITANDO CON PHP

---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- <?php
+  echo "hola mundo";
+?>  -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width= , initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    h1
+    {
+      color:aquamarine;
+    }
+    div
+    {
+      background-color: blue;  
+    }
+  </style>
+</head>
+<body>
+    <h1>hola crayola </h1>
+    <h2>hola crayola</h2>
+    <h3>hola crayola</h3>
+    <h4>hola crayola</h4>
+    <h5>hola crayola</h5>
+    <h6>hola crayola</h6
+    >
+    <div>
+      <h2>
+        AIMEP3 EMBARRAZADA
+      </h2>
+    </div>
+</body>
+</html>
