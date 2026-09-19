1569f2355ee3ee4eb5c500ee63c780481ab0fb63
Aaron Eduardo <125054686@upq.edu.mx>
2026-09-18 19:12:45 -0600

    terminando practica 2 de php y bordes

--- index.php
+++ index.php
@@ -1,22 +1,61 @@
-<!-- <?php
-  echo "hola mundo";
-?>  -->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width= , initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <style>
     h1
     {
-      color:aquamarine;
+      color:green;
     }
     div
     {
-      background-color: blue;  
+      background-color: red;  
     }
+ 
+    .seccion
+    {
+      background-color: green;
+      padding: 20px 40px;
+    }
+ 
+    .seccion2
+    {
+      background-color: lightgray;
+      padding-top: 20px;
+      padding-right: 40px;
+      padding-bottom: 20px;
+      padding-left: 40px;
+    }
+ 
+    .seccion3
+    {
+      background-color: red;
+      padding: 20px 40px;
+      margin: 20px;
+    }
+ 
+    .seccion4
+    {
+      background-color: green;
+      padding: 20px 40px;
+      margin-top: 20px;
+      margin-right: 30px;
+      margin-bottom: 40px;
+      margin-left: 50px;
+    }
+ 
+    .borde1 { background-color: lightgray; border: 3px solid black; }
+    .borde2 { background-color: green; border: 3px dashed black; }
+    .borde3 { background-color: red; border: 3px dotted black; }
+    .borde4 { background-color: lightgray; border: 3px double black; }
+    .borde5 { background-color: green; border: 3px groove black; }
+    .borde6 { background-color: red; border: 3px ridge black; }
+    .borde7 { background-color: lightgray; border: 3px inset black; }
+    .borde8 { background-color: green; border: 3px outset black; }
+    .borde9 { background-color: red; border: 3px none black; }
+    .borde10 { background-color: lightgray; border: 3px hidden black; }
   </style>
 </head>
 <body>
@@ -25,12 +64,67 @@
     <h3>hola crayola</h3>
     <h4>hola crayola</h4>
     <h5>hola crayola</h5>
-    <h6>hola crayola</h6
-    >
+    <h6>hola crayola</h6>
     <div>
       <h2>
-        AIMEP3 EMBARRAZADA
+        HOLAAAAAAAAAAAAAAAAAAAA MUNDO
       </h2>
     </div>
+ 
+    <div class="seccion">
+      <h2>Título del párrafo</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa qui officia deserunt mollit anim id est laborum</p>
+    </div>
+ 
+    <div class="seccion2">
+      <h2>Título del párrafo</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa qui officia deserunt mollit anim id est laborum</p>
+    </div>
+ 
+    <div class="seccion3">
+      <h2>Título del párrafo</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa qui officia deserunt mollit anim id est laborum</p>
+    </div>
+ 
+    <div class="seccion4">
+      <h2>Título del párrafo</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+      nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+      reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+      pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+      culpa qui officia deserunt mollit anim id est laborum</p>
+    </div>
+ 
+    <div class="borde1">border solid</div>
+    <div class="borde2">border dashed</div>
+    <div class="borde3">border dotted</div>
+    <div class="borde4">border double</div>
+    <div class="borde5">border groove</div>
+    <div class="borde6">border ridge</div>
+    <div class="borde7">border inset</div>
+    <div class="borde8">border outset</div>
+    <div class="borde9">border none</div>
+    <div class="borde10">border hidden</div>
 </body>
 </html>
+ 
